28aaf09b21ebf581c2417472b79aee902bac1835
Alex Scotland <alex@scotland.ca>
2024-11-08 13:11:17 -0500

    feat(routers): Adds dynamic router discovery

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -9,13 +9,18 @@ __globals = globals()
 
 from routers.main import MAIN_ROUTER
 
-# Dynamically import all routers
-for file in os.listdir(os.path.dirname(__file__)):
-    if file.startswith("__") or file == "main.py":
-        continue
-    __globals[file[:-3]] = importlib.import_module(f'.{file[:-3]}', package=__name__)
-    MAIN_ROUTER.include_router(__globals[f"{file[:-3]}"].ROUTER)
+def _get_routes_from_package_file(package_file):
+    all_routers_to_import = []
+    for obj_attribute in dir(package_file):
+        if obj_attribute.startswith("__"):
+            continue
+        imported_attribute = getattr(package_file, obj_attribute)
+        if isinstance(imported_attribute, APIRouter) and getattr(imported_attribute, 'routes'):
+            all_routers_to_import.append(imported_attribute)
+    return all_routers_to_import
 
+# Dynamically import all routers
 for package_routers in ALL_ROUTERS_TO_INSTALL:
     __globals[package_routers[:-3]] = importlib.import_module(package_routers[:-3], package=__name__)
-    MAIN_ROUTER.include_router(__globals[package_routers[:-3]].ROUTER)
+    for router in _get_routes_from_package_file(__globals[package_routers[:-3]]):
+        MAIN_ROUTER.include_router(router)
